e7c4f6c990aafcce43f88df166e28249fdac3c0a
krisu <kdemczynski@wp.pl>
2017-02-12 13:22:26 +0100

    some CSS, HTML fixes

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import CardItems, Card, Dish, Categories
+from django.utils import timezone
 
-# Create your tests here.
+
+class CardTestCase(TestCase):
+    def setUp(self):
+        Card.objects.create(name="Test", description="Karta utworzona na potrzeby testow")
+
+    def test_card_query(self):
+        card = Card.objects.get(name="Test")
+        self.assertEqual(card.description, 'Karta utworzona na potrzeby testow')
+
+    def test_card_timezone(self):
+        card = Card.objects.get(name="Test")
+        self.assertEqual(card.created, timezone.now())
